ff4d3a657ccbfa5ed905e7a98ac7e72ba2e655bc
ThanhVT <vutienthanh0210@gmail.com>
2025-05-03 11:46:10 +0700

    Remove

--- metro.config.js
+++ metro.config.js
@@ -15,14 +15,6 @@ module.exports = (async () => {
     resolver: {
       ...defaultConfig.resolver,
       sourceExts: ['jsx', 'js', 'ts', 'tsx', 'json', 'cjs', 'mjs'],
-      blockList: [
-        // Block the problematic PieChart module from react-native-gifted-charts
-        /node_modules\/react-native-gifted-charts\/dist\/PieChart\/.*/,
-      ],
-      extraNodeModules: {
-        // Provide empty implementations for problematic modules
-        'react-native-svg': require.resolve('./src/components/EmptySvg'),
-      },
     },
     transformer: {
       ...defaultConfig.transformer,
